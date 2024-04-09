80fe4dae5cea3801cf7a047cf3201b92f3015630
igijarosz <igijarosz@gmail.com>
2024-04-09 19:42:56 +0200

    Fov

--- game_map.py
+++ game_map.py
@@ -9,9 +9,22 @@ class GameMap:
         self.width, self.height = width, height
         self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")  # Wypełnia mapę ścianami
 
+        self.visible = np.full((width, height), fill_value=False, order="F")  # aktualnie widoczne
+        self.explored = np.full((width, height), fill_value=False, order="F")  # widziane wcześniej
     def in_bounds(self, x: int, y: int) -> bool:
         """True, jeżeli x i y są w obszarze mapy"""
         return 0 <= x < self.width and 0 <= y < self.height
 
     def render(self, console: Console) -> None :
-        console.rgb[0:self.width, 0:self.height] = self.tiles["dark"]
+        """
+        Renderuje mape
+
+        Jeżeli miejsce jest w tablicy 'visible' renderowana jest jako 'light'
+        jeżeli jest w tablicy 'explored' renderowana jest jako 'dark'
+        w innym wypadku (nie została jeszcze zobaczona) renderowana jako SHROUD
+        """
+        console.rgb[0:self.width, 0:self.height] = np.select(
+            condlist=[self.visible, self.explored],
+            choicelist=[self.tiles["light"], self.tiles["dark"]],
+            default=tile_types.SHROUD
+        )
--- tile_types.py
+++ tile_types.py
@@ -14,6 +14,7 @@ tile_dt = np.dtype(
         ("walkable", bool),  # True, jeśli da się poruszać
         ("transparent", bool),  # True, jeśli nie blokuje pola widzenia
         ("dark", graphic_dt),  # Kolor, jeżeli nie jest w polu widzenia
+        ("light", graphic_dt),  # Kolor, jeżeli jest w polu widzenia
     ]
 )
 
@@ -23,14 +24,25 @@ def new_tile(
         walkable: int,
         transparent: int,
         dark: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
+        light: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
 ) -> np.ndarray:
-    return np.array((walkable, transparent, dark), dtype=tile_dt)
+    return np.array((walkable, transparent, dark, light), dtype=tile_dt)
 
+
+# SHROUD -- jeszcze niezobaczone miejsce
+SHROUD = np.array((ord(" "), (255, 255, 255), (0, 0, 0)), dtype=graphic_dt)
 
 floor = new_tile(
-    walkable=True, transparent=True, dark=(ord("."), (100,100,100), (20,20,20))
+    walkable=True,
+    transparent=True,
+    dark=(ord("."), (100, 100, 100), (0, 0, 0)),
+    light=(ord("."), (200, 200, 200), (0, 0, 0))
+
 )
 
 wall = new_tile(
-    walkable=False, transparent=False, dark=(ord("#"), (100,100,100), (20,20,20))
+    walkable=False,
+    transparent=False,
+    dark=(ord("#"),(100, 100, 100), (0, 0, 0)),
+    light=(ord("#"),(200, 200, 200), (0, 0, 0))
 )
